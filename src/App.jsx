39b91e15a84b5bc670a5467b0c05bf0867e7e921
Araham7 <arahamabeddin7@gmail.com>
2025-02-09 19:35:00 +0530

    Add gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 import './App.css';
-// import Layout from './Layout/Layout';
-import { Route, Routes } from "react-router-dom";
+import { Route, BrowserRouter  } from "react-router-dom";
 import HomePage from './Pages/HomePage';
 import Contact from './Pages/Contact';
 import About from './Pages/About';
 import NotFound from './Pages/NotFound';
 import Timetable from './Pages/TimeTable';
-import ExploreCourse from './Pages/ExploreCourse'
+import ExploreCourse from './Pages/ExploreCourse';
 
 function App() {
   return (
     <>
-    <Routes>
+    <BrowserRouter basename="/BMA" >
     <Route path="/" element={<HomePage />} />
     <Route path="/about" element={<About />} />
     <Route path="/contact" element={<Contact />} />
@@ -20,7 +19,7 @@ function App() {
 
     {/* Route to handal: 404 Not-Found Page */}
     <Route path="*" element={<NotFound />} />
-    </Routes>
+    </BrowserRouter >
     </>
   );
 }
